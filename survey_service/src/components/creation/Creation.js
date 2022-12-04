40a70cdca077258b4475e5b32440b67f27a17a84
kier fretenborouh <mr.fretenborough@outlook.com>
2022-12-03 19:14:47 -0500

    checkind to see update:

--- survey_service/src/components/creation/Creation.js
+++ survey_service/src/components/creation/Creation.js
@@ -3,7 +3,7 @@
 function Creation() {
     return (
         <div>
-            This is where we need to put the survey creation stuff
+            This is where we need to put the survey creation stuff. and kier is doing more testing.
         </div>
     )
 }
