5109cfd7d3ecbfd0cff74e525c404f4344c40065
kier fretenborouh <mr.fretenborough@outlook.com>
2022-12-04 02:19:38 -0500

    testing axios call

--- survey_service/src/components/app/App.js
+++ survey_service/src/components/app/App.js
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import Axios from 'axios';
 import './App.css';
 import Login from '../login/Login';
 import Creation from '../creation/Creation';
@@ -8,12 +9,20 @@ import Viewing from '../viewing/Viewing';
 function App() {
   const [view, setView] = useState(0);
 
+  const createUser = () => {
+    Axios.get("http://18.207.227.234:3002/testing_api", {
+      val: view
+    }).then(() => {
+      alert("this is an alert, ew")
+    });
+  }
+
   return (
     <div className="App">
       {
         view != 0 &&
         <h1 className="header">
-          <button className="button" onClick={() => setView(1)}>Survey Creation</button>
+          <button className="button" onClick={createUser}>Survey Creation</button>
           <button className="button" onClick={() => setView(2)}>Survey Participation</button>
           <button className="button" onClick={() => setView(3)}>Survey Results</button>
         </h1>
