a20f7cbfdf1337550f30e9ce8c7371694e3920c3
kier fretenborouh <mr.fretenborough@outlook.com>
2022-12-03 22:58:31 -0500

    testing server index.js

--- server/index.js
+++ server/index.js
@@ -7,10 +7,6 @@ const PORT = 443;
 app.use(cors());
 app.use(express.json())
 
-app.get("/test", (req, res) => {
-    res.json("sup jit");
-})
-
 app.listen(PORT, ()=>{
     console.log(`Server is running on ${PORT}`)
 })
