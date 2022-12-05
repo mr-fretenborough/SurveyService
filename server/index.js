0e66cb8ab5c6d364fecad81f1e868ac051ef6c88
Rsegui22 <106122697+Rsegui22@users.noreply.github.com>
2022-12-05 17:32:21 -0500

    creation api and page

--- server/index.js
+++ server/index.js
@@ -64,19 +64,39 @@ app.post('/authenticate_user', (req, res) => {
 });
 /************************** Create Survey **************************/
 app.post('/create_survey', (req, res) => {
-    // format new user data & build query
-    const email = req.body.email;
-    const password = req.body.password;
-    const query = `
-        insert into Users (Email, Password, Verified) values (${email}, ${password}, default);
+    // get survey data
+    const userid = req.body.user_id;
+    const title = req.body.title;
+    const description = req.body.description;
+    const startDate = req.body.startDate;
+    const endDate = req.body.endDate;
+
+    // get question data
+    const sid = req.body.survey_id;
+    const questions = req.body.questions;
+    const questionType = req.body.questionType;
+
+    const survey_query = `
+        insert into  Surveys (UserID, Title, Description, StartDate, EndDate) values (${userid}, ${title}, ${description}, ${startDate}, ${endDate})
     `;
+
+    const questions_query = `
+        insert into Questions (SurveyID, QuestionType, Question) values (${sid}, ${questionType}, ${questions})
+    `
     // execute sql
-    db.query(query, (err, out) => {
+    db.query(survey_query, (err, out) => {
         if (err) {
             console.log(err);
         }
         console.log(out);
-        res.send("inside");
+        res.send("survey info posted");
+    });
+    db.query(questions_query, (err, out) => {
+        if (err) {
+            console.log(err);
+        }
+        console.log("questions posted");
+        res.send(out);
     });
 });
 /************************** Get Surveys **************************/
--- survey_service/src/components/creation/Creation.js
+++ survey_service/src/components/creation/Creation.js
@@ -1,11 +1,148 @@
+import { useState } from "react";
+import Axios from "axios";
+import "./Creation.css";
 
 
-function Creation() {
+function Creation(props) {
+    const [title, setTitle] = useState("");
+    const [start, setStart] = useState("");
+    const [end, setEnd] = useState("");
+    const [description, setDescription] = useState("");
+    const [questionList, setQuestionList] = useState([{ question: "" }]);
+    const [typeValue, setTypeValue] = useState("");
+
+    console.log(questionList);
+
+    const createSurvey = () => {
+        Axios.post(`${props.host}:3002/create_survey`, {
+            body: {
+                title: title,
+                startDate: start,
+                endDate: end,
+                description: description,
+                questionList: {
+                  question: questionList,
+                  typeValue: typeValue
+                }
+            }
+        }).then(res => {
+            console.log(res);
+            console.log(res.data);
+        })
+    }
+
+    const handleQuestionChange = (e, index) => {
+      const { name, value } = e.target;
+      const list = [...questionList];
+      list[index][name] = value;
+      setQuestionList(list);
+    };
+  
+    const handleQuestionRemove = (index) => {
+      const list = [...questionList];
+      list.splice(index, 1);
+      setQuestionList(list);
+    };
+  
+    const handleQuestionAdd = () => {
+      setQuestionList([...questionList, { question: "" }]);
+    };
+  
     return (
+        <form className="App" autoComplete="off">
         <div>
-            This is where we need to put the survey creation stuff. and kier is doing more testing.
+            <label> 
+                Title
+                <input 
+                    type="text"
+                    id="title"
+                    name="title"
+                    onChange={(e) => setTitle(e.target.value)}
+                    required
+                />
+            </label>
         </div>
-    )
+        <div>
+            <label> 
+                Description
+                <input 
+                    type="text"
+                    id="description"
+                    name="description"
+                    onChange={(e) => setDescription(e.target.value)}
+                    required
+                />
+            </label>
+        </div>
+        <div>
+            <label> 
+                Start: <input 
+                    type="text" 
+                    id="start" 
+                    name="start" 
+                    onChange={(e) => setStart(e.target.value)}
+                    required/>
+                End: <input 
+                    type="text" 
+                    id="end" 
+                    name="end" 
+                    onChange={(e) => setEnd(e.target.value)} 
+                    required/>
+            </label>
+        </div>
+        <div className="form-field">
+          <label htmlFor="question">Question(s)</label>
+          {questionList.map((singleQuestion, index) => (
+            <div key={index} className="questions">
+              <div className="first-division">
+                <input
+                  name="question"
+                  type="text"
+                  id="question"
+                  value={singleQuestion.question}
+                  onChange={(e) => handleQuestionChange(e, index)}
+                  required
+                />
+                {questionList.length - 1 === index && (
+                  <button
+                    type="button"
+                    onClick={handleQuestionAdd}
+                    className="add-btn"
+                  >
+                    <span>Add question</span>
+                  </button>
+                )}
+              </div>
+              <div>
+                <label>
+                    Select question type:
+                    <select 
+                        name="typeValue" 
+                        id="typeValue"
+                        onChange={(e) => setTypeValue(e.target.value)}
+                    > 
+                        <option value = "type 1">Free Response</option>
+                        <option value = "type 2">Rating</option>
+                    </select>
+                </label>
+                </div>
+              <div className="second-division">
+                {questionList.length !== 1 && (
+                  <button
+                    type="button"
+                    onClick={() => handleQuestionRemove(index)}
+                    className="remove-btn"
+                  >
+                    <span>Remove</span>
+                  </button>
+                )}
+              </div>
+            </div>
+          ))}
+        </div>
+        <button type="button" onClick={() => createSurvey()}> <span>Submit</span> </button>
+      </form>
+    );
 }
 
 export default Creation;
